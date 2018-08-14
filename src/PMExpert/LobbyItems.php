a0659d93e2055c131beb9c8d0df247c6c7570120
chaosfreak93 <36674649+chaosfreak93@users.noreply.github.com>
2018-08-14 16:34:31 +0200

    Add Hub Command, Add Config

--- src/PMExpert/LobbyItems.php
+++ src/PMExpert/LobbyItems.php
@@ -8,7 +8,28 @@ use muqsit\invmenu\InvMenuHandler;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat as Color;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 
 class LobbyItems extends PluginBase implements Listener {
 	public $prefix = Color::GRAY . "» " . Color::RED . Color::BOLD . "LobbyItems". Color::RESET . Color::GRAY . " » ";
+	
+	public function onEnable() {
+		$this->getLogger->info($this->prefix . "\nAddon geladen");
+		$this->config = $this->getConfig();
+		$this->config = new Config($this->getDataFolder()."settings.yml", Config::YAML, [
+		  "IP" => "",
+		  "Port" => ,
+		]);
+		}
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
+    	$this->config = $this->getConfig();
+        $this->config = new Config($this->getDataFolder() . "settings.yml", Config::YAML);
+    	if($command->getName === "hub"){
+    	$ip = $this->config->get("IP");
+        $port = $this->config->get("Port");
+        $sender->transfer($ip, $port);
+        
+    }
+    }
 	}
