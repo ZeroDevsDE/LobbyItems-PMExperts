f04fdd7cf4865d7e04521d05e7840c7680de38dd
Chaosfreak93 <yanluca.lentzen@gmail.com>
2018-08-14 19:12:33 +0200

    Fixed Commands and Configs

--- src/PMExpert/LobbyItems.php
+++ src/PMExpert/LobbyItems.php
@@ -10,36 +10,35 @@ use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat as Color;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\utils\Config;
 
 class LobbyItems extends PluginBase implements Listener {
 	public $prefix = Color::GRAY . "» " . Color::RED . Color::BOLD . "LobbyItems". Color::RESET . Color::GRAY . " » ";
 	
 	public function onEnable() {
-		$this->getLogger->info($this->prefix . "\nAddon geladen");
+		$this->getLogger()->info($this->prefix . "\nAddon geladen");
 		$this->config = $this->getConfig();
 		$this->config = new Config($this->getDataFolder()."settings.yml", Config::YAML, [
-		  "IP" => "127.0.0.1",
+		  "IP" => "91.200.100.50",
 		  "Port" => 19132,
-		"##You can switch between UI or Hotbar",
-		  "Compass" => UI,
+		  "Compass" => "UI",
 		]);
-		if($this->config->get("Compass") === UI){
+		if($this->config->get("Compass") === "UI"){
 			$this->config = new Config($this->getDataFolder()."compass.yml", Config::YAML, [
 			"item0" => 0,
-			"x" => '',
-			"y" => '',
-			"z" => '',
+			"x0" => 0,
+			"y0" => 0,
+			"z0" => 0,
 			"item1" => 0,
-			"x" => '',
-			"y" => '',
-			"z" => '',
+			"x1" => 0,
+			"y1" => 0,
+			"z1" => 0,
 			"item2" => 0,
-			"x" => '',
-			"y" => '';
-			"z" => '',
-		    
+			"x2" => 0,
+			"y2" => 0,
+			"z2" => 0,
 			]);
-			} else if($this->config->get("Compass") === Hotbar) {
+			} else if($this->config->get("Compass") === "Hotbar") {
 				
 				}
 		}
@@ -47,15 +46,16 @@ class LobbyItems extends PluginBase implements Listener {
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
     	$this->config = $this->getConfig();
         $this->config = new Config($this->getDataFolder() . "settings.yml", Config::YAML);
-    	if($command->getName === "hub"){
+    	if($command->getName() === "hub"){
     	$ip = $this->config->get("IP");
         $port = $this->config->get("Port");
         $sender->transfer($ip, $port);
     }
-    if($command->getName === "lobby"){
+    if($command->getName() === "lobby"){
     $ip = $this->config->get("IP");
     $port = $this->config->get("Port");
     $sender->transfer($ip, $port);
     }
+    return false;
     }
 	}
