99f78110462692d4ea8a80ddc71446a649b529ac
Chaosfreak93 <yanluca.lentzen@gmail.com>
2018-08-15 07:01:05 +0200

    Finish setup Config, Add First Item(Compass)

--- src/PMExpert/LobbyItems.php
+++ src/PMExpert/LobbyItems.php
@@ -36,6 +36,26 @@ class LobbyItems extends PluginBase implements Listener {
 			"x3" => 0,
 			"y3" => 0,
 			"z3" => 0,
+			"item4" => 0,
+			"x4" => 0,
+			"y4" => 0,
+			"z4" => 0,
+			"item5" => 0,
+			"x5" => 0,
+			"y5" => 0,
+			"z5" => 0,
+			"item6" => 0,
+			"x6" => 0,
+			"y6" => 0,
+			"z6" => 0,
+			"item7" => 0,
+			"x7" => 0,
+			"y7" => 0,
+			"z7" => 0,
+			"item8" => 0,
+			"x8" => 0,
+			"y8" => 0,
+			"z8" => 0,
 			]);
 		}
 		
@@ -53,5 +73,10 @@ class LobbyItems extends PluginBase implements Listener {
     $sender->transfer($ip, $port);
     }
     return false;
+    }
+    public function mainItems(Player $player) {
+    $compass = Item:get(345 , 0, 1);
+    $compass->setCustomName(Color::GRAY . "[" . Color::GREEN . "Compass" . Color::GRAY . "]");
+    $player->setItem(0, $compass);
     }
 	}
