fd76e5364adba50cb44d812b88436e7bcdd132a3
Chaosfreak93 <yanluca.lentzen@gmail.com>
2018-08-16 15:00:40 +0200

    Update Config Added resources Ordner

--- src/PMExpert/LobbyItems.php
+++ src/PMExpert/LobbyItems.php
@@ -25,38 +25,47 @@ class LobbyItems extends PluginBase implements Listener {
 		]);
 			$this->config = new Config($this->getDataFolder()."compass.yml", Config::YAML, [
 			"item0" => 0,
+			"name0" => Test,
 			"x0" => 0,
 			"y0" => 0,
 			"z0" => 0,
 			"item1" => 0,
+			"name1" => Test,
 			"x1" => 0,
 			"y1" => 0,
 			"z1" => 0,
 			"item2" => 0,
+			"name2" => Test,
 			"x2" => 0,
 			"y2" => 0,
 			"z2" => 0,
 			"item3" => 0,
+			"name3" => Test,
 			"x3" => 0,
 			"y3" => 0,
 			"z3" => 0,
 			"item4" => 0,
+			"name4" => Test,
 			"x4" => 0,
 			"y4" => 0,
 			"z4" => 0,
 			"item5" => 0,
+			"name5" => Test,
 			"x5" => 0,
 			"y5" => 0,
 			"z5" => 0,
 			"item6" => 0,
+			"name6" => Test,
 			"x6" => 0,
 			"y6" => 0,
 			"z6" => 0,
 			"item7" => 0,
+			"name7" => Test,
 			"x7" => 0,
 			"y7" => 0,
 			"z7" => 0,
 			"item8" => 0,
+			"name8" => Test,
 			"x8" => 0,
 			"y8" => 0,
 			"z8" => 0,
@@ -77,6 +86,10 @@ class LobbyItems extends PluginBase implements Listener {
     $sender->transfer($ip, $port);
     }
     return false;
+    }
+    public function games(Player $player) {
+    	
+    
     }
     
     public function mainItems(Player $player) {
