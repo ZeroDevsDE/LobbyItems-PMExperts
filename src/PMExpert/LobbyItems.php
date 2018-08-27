8b53c54db2c76674fbdbb4884aa49fac1281e36a
Chaosfreak93 <yanluca.lentzen@gmail.com>
2018-08-27 07:00:39 +0200

    First three items made editable by config

--- src/PMExpert/LobbyItems.php
+++ src/PMExpert/LobbyItems.php
@@ -10,89 +10,104 @@ use pocketmine\command\CommandSender;
 use pocketmine\utils\Config;
 use pocketmine\Player;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerRespawnEvent;
 use pocketmine\item\Item;
+use pocketmine\level\Position;
 
 class LobbyItems extends PluginBase implements Listener {
 	public $prefix = Color::GRAY . "» " . Color::RED . Color::BOLD . "LobbyItems". Color::RESET . Color::GRAY . " » ";
 	
 	public function onEnable() {
-		$this->getLogger()->info($this->prefix . "\nAddon geladen");
+		$this->saveResource("compass.yml");
+		$this->saveResource("settings.yml");
+		$this->getLogger()->info("\n" . $this->prefix . "Addon geladen");
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->config = $this->getConfig();
-		$this->config = new Config($this->getDataFolder()."settings.yml", Config::YAML, [
+		$settings = new Config($this->getDataFolder()."settings.yml", Config::YAML, [
 		  "IP" => "91.200.100.50",
 		  "Port" => 19132,
 		]);
-			$this->config = new Config($this->getDataFolder()."compass.yml", Config::YAML, [
-			"item0" => 0,
-			"name0" => Test,
+		$compass = new Config($this->getDataFolder()."compass.yml", Config::YAML, [
+		  "item0" => 0,
+			"name0" => "Test",
 			"x0" => 0,
 			"y0" => 0,
 			"z0" => 0,
 			"item1" => 0,
-			"name1" => Test,
+			"name1" => "Test",
 			"x1" => 0,
 			"y1" => 0,
 			"z1" => 0,
 			"item2" => 0,
-			"name2" => Test,
+			"name2" => "Test",
 			"x2" => 0,
 			"y2" => 0,
 			"z2" => 0,
 			"item3" => 0,
-			"name3" => Test,
+			"name3" => "Test",
 			"x3" => 0,
 			"y3" => 0,
 			"z3" => 0,
 			"item4" => 0,
-			"name4" => Test,
+			"name4" => "Test",
 			"x4" => 0,
 			"y4" => 0,
 			"z4" => 0,
 			"item5" => 0,
-			"name5" => Test,
+			"name5" => "Test",
 			"x5" => 0,
 			"y5" => 0,
 			"z5" => 0,
 			"item6" => 0,
-			"name6" => Test,
+			"name6" => "Test",
 			"x6" => 0,
 			"y6" => 0,
 			"z6" => 0,
 			"item7" => 0,
-			"name7" => Test,
+			"name7" => "Test",
 			"x7" => 0,
 			"y7" => 0,
 			"z7" => 0,
-			"item8" => 0,
-			"name8" => Test,
-			"x8" => 0,
-			"y8" => 0,
-			"z8" => 0,
 			]);
 		}
 		
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
-    	$this->config = $this->getConfig();
-        $this->config = new Config($this->getDataFolder() . "settings.yml", Config::YAML);
+        $settings = new Config($this->getDataFolder() . "settings.yml", Config::YAML);
     	if($command->getName() === "hub"){
-    	$ip = $this->config->get("IP");
-        $port = $this->config->get("Port");
+    	$ip = $settings->get("IP");
+        $port = $settings->get("Port");
         $sender->transfer($ip, $port);
     }
     if($command->getName() === "lobby"){
-    $ip = $this->config->get("IP");
-    $port = $this->config->get("Port");
+    $ip = $settings->get("IP");
+    $port = $settings->get("Port");
     $sender->transfer($ip, $port);
     }
     return false;
     }
-    public function games(Player $player) {
-    	
+    public function games(Player $player){
+    $compass = new Config($this->getDataFolder()."compass.yml", Config::YAML);
+    $player->getInventory()->clearAll();
+    $item0 = Item::get($compass->get("item0"));
+    $item0->setCustomName($compass->get("name0"));
+    $player->getInventory()->setItem(0, $item0);
+    $item1 = Item::get($compass->get("item1"));
+    $item1->setCustomName($compass->get("name1"));
+    $player->getInventory()->setItem(1, $item1);
+    $item2 = Item::get($compass->get("item2"));
+    $item2->setCustomName($compass->get("name2"));
+    $player->getInventory()->setItem(2, $item2);
+    $item3 = Item::get($compass->get("item3"));
+    $item3->setCustomName($compass->get("name3"));
+    $player->getInventory()->setItem(3, $item3);
+    $back = Item::get(372);
+    $back->setCustomName(Color::GRAY . "[" . Color::DARK_RED . "Back" . Color::GRAY . "]");
+    $player->getInventory()->setItem(8, $back);
     
     }
     
     public function mainItems(Player $player) {
+    $player->getInventory()->clearAll();
     $compass = Item::get(345, 0, 1);
     $compass->setCustomName(Color::GRAY . "[" . Color::GREEN . "Compass" . Color::GRAY . "]");
     $player->getInventory()->setItem(0, $compass);
@@ -100,6 +115,42 @@ class LobbyItems extends PluginBase implements Listener {
     
     public function onJoin(PlayerJoinEvent $event) {
     	$player = $event->getPlayer();
+        $player->getInventory()->clearAll();
     	$this->mainItems($player);
     }
-	}
+    
+    public function onRespawn(PlayerRespawnEvent $event) {
+    	$player = $event->getPlayer();
+        $this->mainItems($player);
+    }
+	
+	public function onInteract(PlayerInteractEvent $event) {
+        $compass = new Config($this->getDataFolder()."compass.yml", Config::YAML);
+		$player = $event->getPlayer();
+		$item = $player->getInventory()->getItemInHand();
+		if($item->getCustomName() === Color::GRAY . "[" . Color::GREEN . "Compass" . Color::GRAY . "]") {
+			$this->games($player);
+		} else if($item->getCustomName() === Color::GRAY . "[" . Color::DARK_RED . "Back" . Color::GRAY . "]") {
+			$this->mainItems($player);
+		} else if($item->getCustomName() === $compass->get("name0")) {
+			$x0 = $compass->get("x0");
+			$y0 = $compass->get("y0");
+			$z0 = $compass->get("z0");
+			$level = $player->getLevel();
+			$player->teleport(new Position($x0, $y0, $z0, $level));
+		} else if($item->getCustomName() === $compass->get("name1")) {
+			$x1 = $compass->get("x1");
+			$y1 = $compass->get("y1");
+			$z1 = $compass->get("z1");
+			$level = $player->getLevel();
+			$player->teleport(new Position($x1, $y1, $z1, $level));
+		} else if($item->getCustomName() === $compass->get("name2")) {
+			$x2 = $compass->get("x2");
+			$y2 = $compass->get("y2");
+			$z2 = $compass->get("z2");
+			$level = $player->getLevel();
+			$player->teleport(new Position($x2 ,$y2, $z2, $level));
+			}
+		
+		}
+		}
